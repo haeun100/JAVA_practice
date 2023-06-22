67ff569f008f0e50c5ca92dbfe9659ec83a672ea
haeun100 <137380633+haeun100@users.noreply.github.com>
2023-06-22 16:57:42 +0900

    0411

---
+++ Ch06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ch06 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner keyboard=new Scanner(System.in);
+		
+		int aNum=7;
+		if((aNum>0)&&((++aNum/3)>0)) {
+			aNum++;
+		}
+		System.out.println("aNum = "+aNum);
+		
+	}	
+}
+
